6008ed89dea8606042796114b7928f4b893d862d
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2023-10-26 23:43:44 -0700

    model integration + api endpoints

--- Website/API.py
+++ Website/API.py
@@ -1,6 +1,70 @@
 from flask import Blueprint, jsonify, request, redirect
-
+import pickle
+import os
 API = Blueprint('API', __name__)
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+assets_directory = os.path.join(current_directory, 'Assets/Model')
+imputer_file_path = os.path.join(assets_directory, 'imputer.pkl')
+model_file_path = os.path.join(assets_directory, 'model.pkl')
+
+with open(model_file_path, "rb") as model_file:
+    model = pickle.load(model_file)
+
+with open(imputer_file_path, "rb") as imputer_file:
+    imputer = pickle.load(imputer_file)
+
+
+def reformat_input(content):
+    feature_map = {
+        'SEX': {'male': 0, 'female': 1},
+        'DIABETES': {'yes': 1, 'no': 0},
+        'OBESITY': {'yes': 1, 'no': 0},
+        'ASTHMA': {'yes': 1, 'no': 0},
+        'TOBACCO': {'yes': 1, 'no': 0},
+        'PATIENT_TYPE': {'yes': 1, 'no': 0}
+    }
+    
+    reformatted_content = {}
+    for feature, mapping in feature_map.items():
+        value = content.get(feature, '').lower()
+        reformatted_content[feature] = mapping.get(value, None)
+        
+    reformatted_content['AGE'] = int(content.get('AGE', 0))
+
+    return reformatted_content
+
+
+@API.route("/api/predict", methods=["POST"])
+def predict():
+    try:
+        #example input {"SEX": "male", "AGE": 20, "DIABETES": "yes", "OBESITY": "yes", "ASTHMA": "yes", "TOBACCO": "yes", "PATIENT_TYPE": "no"}
+        # Parse input features from JSON
+        content = request.json
+        reformatted_content = reformat_input(content)
+        
+        feature_array = [
+            reformatted_content['SEX'],
+            reformatted_content['AGE'],
+            reformatted_content['DIABETES'],
+            reformatted_content['OBESITY'],
+            reformatted_content['ASTHMA'],
+            reformatted_content['TOBACCO'],
+            reformatted_content['PATIENT_TYPE']
+        ]
+        # Handle missing values using the imputer
+        feature_array = imputer.transform([feature_array])
+
+        # Make prediction
+        prediction = model.predict_proba(feature_array)
+        
+        # Return probability (the second class)
+        return jsonify({"probability": prediction[0][1]})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
 
 @API.route('/api/test', methods=['GET'])
 def test():
@@ -15,6 +79,7 @@ def login():
     if data.get("username") == VALID_USERNAME and data.get("password") == VALID_PASSWORD:
         return jsonify(userAuthenticated="true"), 200
         
+
 
 
 if __name__ == '__main__':
--- Website/ML_Models.py
+++ Website/ML_Models.py
@@ -1,1 +1,63 @@
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.impute import SimpleImputer  # Import the SimpleImputer
+import os
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+# Construct the path to the Assets directory
+assets_directory = os.path.join(current_directory, 'Assets/Model')
+
+# Now specify the complete path to the file where you want to store the imputer
+imputer_file_path = os.path.join(assets_directory, 'imputer.pkl')
+model_file_path = os.path.join(assets_directory, 'model.pkl')
+data_set = os.path.join(assets_directory, 'Covid Data.csv')
+# Load data and handle missing values
+data = pd.read_csv(data_set);
+data.replace({97: np.nan, 99: np.nan}, inplace=True)
+
+# Encode categorical features
+data['SEX'] = (data['SEX'] == 2).astype(int)
+data['DIABETES'] = (data['DIABETES'] == 1).astype(int)
+data['OBESITY'] = (data['OBESITY'] == 1).astype(int)
+data['ASTHMA'] = (data['ASTHMA'] == 1).astype(int)
+data['TOBACCO'] = (data['TOBACCO'] == 1).astype(int)
+
+data['PATIENT_TYPE']= (data['PATIENT_TYPE'] == 1).astype(int)
+# Select features and target variable
+selected_columns = ['SEX', 'AGE', 'DIABETES', 'OBESITY', 'ASTHMA', 'TOBACCO','PATIENT_TYPE']
+X = data[selected_columns]
+y = (data['CLASIFFICATION_FINAL'] <= 3).astype(int)
+
+# Handle missing values using imputation
+imputer = SimpleImputer(strategy='mean')  # You can change the strategy as needed
+X = imputer.fit_transform(X)
+
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and train the logistic regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Make predictions
+# y_pred = model.predict(X_test)
+
+# Serialize the model to a file
+with open(model_file_path, "wb") as model_file:
+    pickle.dump(model, model_file)
+
+# Serialize the imputer to a file
+with open(imputer_file_path, "wb") as imputer_file:
+    pickle.dump(imputer, imputer_file)
+
+# Calculate and print accuracy
+# accuracy = accuracy_score(y_test, y_pred)
+# print(f"Accuracy: {accuracy * 100:.2f}%")
+
+# Print the classification report
+# print(classification_report(y_test, y_pred))
